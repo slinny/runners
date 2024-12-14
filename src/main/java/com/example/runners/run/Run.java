52ffa273ffd98711ffef65d08f9894746fdfb391
Siran Li <siranli411@gmail.com>
2024-12-14 17:05:41 -0600

    Add validation in Run

--- src/main/java/com/example/runners/run/Run.java
+++ src/main/java/com/example/runners/run/Run.java
@@ -1,12 +1,17 @@
 package com.example.runners.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location
 ) {
